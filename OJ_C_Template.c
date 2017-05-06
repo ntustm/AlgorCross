5658ca3c80a9b035e6a3adc57d09d34d35eee4a0
WillisTrue <m10302811@mail.ntust.edu.tw>
2017-05-06 16:55:42 +0800

    Create OJ_C_Template.c

---
+++ OJ_C_Template.c
@@ -0,0 +1,13 @@
+// Author: Willis Chu
+// Date  : 2017/05/06
+#include <stdio.h>
+
+const unsigned int ARR_SZ 65535
+void main(){
+  char str[ARR_SZ];
+  memset(str, 0, sizeof(char)*ARR_SZ);
+  freopen("testcase00.in", "r", stdin);
+  while(gets(instr)){
+    //BODY  do whatever U want
+  }
+}
