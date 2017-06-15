a6daf5183632961cd0f5f9bdaf8d404f815c85d0
WillisTrue <m10302811@mail.ntust.edu.tw>
2017-06-15 20:47:46 +0800

    Create debug.cpp

--- debug.cpp
+++ debug.cpp
@@ -1,9 +1,11 @@
 #define __DEBUG_ON__ 1
 #if __DEBUG_ON__
-  #define DBOUT cout<<__FILE__<<":"<<__LINE
+  #define DBOUT cout<<__FILE__<<":"<<__LINE__
 #else
   #define DBOUT 0 && cout
 #endif
+
+#define _TRCE_(_LV_) (_LV_<=__DEBUG_ON__)&&cout<<__FILE__<<":"<<__LINE__
 
 int main(int argc, char** argv) {
   do{
